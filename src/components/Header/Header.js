36e1ed930b4e74cadd2c886cd3729606f391f67d
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-09 14:04:14 +0600

    youtube video added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className='m-2'>
-      <Link className='p-2 m-2 text-red-500 text-lg font-semibold' to='/menu'>Menu</Link>
-      <Link className='p-2 m-2 text-red-500 text-lg font-semibold' to='/offer'>Offer</Link>
-      <Link className='p-2 m-2 text-red-500 text-lg font-semibold' to='/career'>Career</Link>
-      <Link className='p-2 m-2 text-red-500 text-lg font-semibold' to='/reward'>Reward</Link>
+      <Link className='p-2 m-2 text-red-500 text-lg hover:font-bold' to='/menu'>Menu</Link>
+      <Link className='p-2 m-2 text-red-500 text-lg hover:font-bold' to='/offer'>Offer</Link>
+      <Link className='p-2 m-2 text-red-500 text-lg hover:font-bold' to='/career'>Career</Link>
+      <Link className='p-2 m-2 text-red-500 text-lg hover:font-bold' to='/reward'>Reward</Link>
     </div>
   );
 };
--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -4,11 +4,21 @@ import { useLoaderData } from 'react-router-dom';
 const MealDetails = () => {
   const mealDetail = useLoaderData();
   console.log(mealDetail.meals[0]);
-  const { strMeal, strInstructions, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20 } = mealDetail.meals[0];
+  const { strMeal, strInstructions, strYoutube, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20 } = mealDetail.meals[0];
+  const sliceYouTube = strYoutube.slice(0, 32);
+  const slicedYouTube = sliceYouTube.replace(sliceYouTube, `https://www.youtube.com/embed/`);
+  // console.log(finalYouTube);
+  const sliceYouTube2 = strYoutube.slice(32, 43);
+  // console.log(sliceYouTube2);
+  const finalYoutube = slicedYouTube + sliceYouTube2;
+  console.log(finalYoutube);
   return (
-    <div>
+    <div className='mt-10'>
       <h1 className='text-3xl text-center'>{strMeal}</h1>
-      <img className='block mx-auto my-5 w-6/12' src={strMealThumb} alt="" />
+      <div className='flex justify-evenly'>
+        <img className='inline-block my-5 w-5/12 rounded-md' src={strMealThumb} alt="" />
+        <iframe className='my-5 rounded-md' width="560" height="526" src={finalYoutube} title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+      </div>
       <div className='w-9/12 mx-auto border-2 border-red-200 p-5 text-lg'>
         <h3 className='text-center text-2xl font-semibold'>Ingredients List</h3>
         <div className='flex justify-between'>
