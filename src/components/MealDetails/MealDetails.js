c569985bc0dc0340275a53772e06249b54036791
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-09 16:04:55 +0600

    redirect added

--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -7,9 +7,9 @@ const MealDetails = () => {
   const { strMeal, strInstructions, strYoutube, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20 } = mealDetail.meals[0];
 
   const sliceYouTube = strYoutube.slice(0, 32);
-  const slicedYouTube = sliceYouTube.replace(sliceYouTube, `https://www.youtube.com/embed/`);
+  const replacedYouTube = sliceYouTube.replace(sliceYouTube, `https://www.youtube.com/embed/`);
   const sliceYouTube2 = strYoutube.slice(32, 43);
-  const finalYoutube = slicedYouTube + sliceYouTube2;
+  const finalYoutube = replacedYouTube + sliceYouTube2;
 
   return (
     <div className='mt-10 pb-20'>
